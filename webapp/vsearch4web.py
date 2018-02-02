ff27bf6813740836d694dd1e87df5db94aae05e1
kfurue <k.furue0728@gmail.com>
2018-02-03 08:21:49 +0900

    Add support for logging requests and results.

--- webapp/vsearch4web.py
+++ webapp/vsearch4web.py
@@ -3,12 +3,18 @@ from vsearch import search4letters
 
 app = Flask(__name__)
 
+def log_request(req: 'flask_request', res: str) -> None:
+    with open('vsearch.log', 'a') as log:
+        print(req, res, file=log)
+
+
 @app.route('/search4', methods=['POST'])
 def do_search() -> 'html':
     phrase = request.form['phrase']
     letters = request.form['letters']
     results = str(search4letters(phrase, letters))
     title = 'Here are your results:'
+    log_request(request, results)
     return render_template('results.html',
                             the_title = title,
                             the_phrase = phrase,
@@ -21,6 +27,13 @@ def do_search() -> 'html':
 def entry_page() -> 'html':
     return render_template('entry.html',
                             the_tite='Welcome to search4letters on the web!')
+
+
+@app.route('/viewlog')
+def view_the_log() -> str:
+    with open('vsearch.log') as log:
+        contents = log.read()
+    return contents
 
 if __name__ == '__main__':
     app.run(debug=True)
