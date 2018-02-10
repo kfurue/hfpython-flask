efeb6cc3acd2fdbb96deaaeab0c3d9da0e232457
kfurue <k.furue0728@gmail.com>
2018-02-11 08:16:20 +0900

    Add support for using DB context manager on /viewlog.

--- webapp/vsearch4web.py
+++ webapp/vsearch4web.py
@@ -1,30 +1,27 @@
 from flask import Flask, render_template, request, escape
 from vsearch import search4letters
-import mysql.connector
+
+from DBcm import UseDatabase
 
 app = Flask(__name__)
 
+app.config['dbconfig'] = { 'host': '127.0.0.1',
+                           'user': 'vsearch',
+                           'password': 'vsearchpasswd',
+                           'database': 'vsearchlogDB', }
+
+
 def log_request(req: 'flask_request', res: str) -> None:
-    dbconfig = { 'host': '127.0.0.1',
-                 'user': 'vsearch',
-                 'password': 'vsearchpasswd',
-                 'database': 'vsearchlogDB', }
-
-    conn = mysql.connector.connect(**dbconfig)
-    _SQL = """insert into log
-              (phrase, letters, ip, browser_string, results)
-              values
-              (%s, %s, %s, %s, %s)"""
-
-    cursor = conn.cursor()
-    cursor.execute(_SQL, (req.form['phrase'],
-                          req.form['letters'],
-                          req.remote_addr,
-                          req.user_agent.browser,
-                          res))
-    conn.commit()
-    cursor.close()
-    conn.close()
+    with UseDatabase(app.config['dbconfig']) as cursor:
+        _SQL = """insert into log
+                  (phrase, letters, ip, browser_string, results)
+                  values
+                  (%s, %s, %s, %s, %s)"""
+        cursor.execute(_SQL, (req.form['phrase'],
+                            req.form['letters'],
+                            req.remote_addr,
+                            req.user_agent.browser,
+                            res))
 
 
 @app.route('/search4', methods=['POST'])
@@ -50,17 +47,16 @@ def entry_page() -> 'html':
 
 @app.route('/viewlog')
 def view_the_log() -> 'html':
-    contents = []
-    with open('vsearch.log') as log:
-        for line in log:
-            contents.append([])
-            for item in line.split('|') :
-                contents[-1].append(escape(item))
-    titles = ('Form data', 'Remote_addr', 'User_agent', 'Results')
-    return render_template('viewlog.html',
-                            the_title='View Log',
-                            the_row_titles=titles,
-                            the_data=contents,)
+    with UseDatabase(app.config['dbconfig']) as cursor:
+        _SQL = """select phrase, letters, ip, browser_string, results
+                  from log"""
+        cursor.execute(_SQL)
+        contents = cursor.fetchall()
+        titles = ('Phrase', 'Letters', 'Remote_addr', 'User_agent', 'Results')
+        return render_template('viewlog.html',
+                                the_title='View Log',
+                                the_row_titles=titles,
+                                the_data=contents)
 
 if __name__ == '__main__':
     app.run(debug=True)
